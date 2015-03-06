9d636039353165e61deaf07f4b9e40b1309da077
Jeff <jeffy9@gmail.com>
2015-03-06 17:36:48 -0500

    testing

--- delphic.litebox.new.js
+++ delphic.litebox.new.js
@@ -4,37 +4,63 @@
 	// http://ryanflorence.com/authoring-jquery-plugins-with-object-oriented-javascript/
 
 	// first we set up our constructor function
-	var Litebox = function($content, config){
-		this.options = $.extend({}, this.defaults, config);
-		this.setup($content, config);
+	var Litebox = function($elm, config){
+		this.init($elm, config);
+		this.attach($elm);
 	};
 
 	// now we define the prototype
 
 	Litebox.prototype = {
 
-		defaults: {},
-
-		setup: function(element){
-			console.log('setup')
-			//this.options.onChange.apply(this, [isEqual, this.elements]);
+		defaults: {
+			contentFilters: ['image', 'html']
 		},
 
+		init: function($elm, config){
+			this.$elm = $elm;
+			this.config = $.extend({}, this.defaults, config);
+			console.log('init');
+
+			var self = this,
+			    $html = $([
+						'<div id="litebox" class="loading">',
+							'<div class="litebox__container">',
+								'<div class="litebox__loading"><div></div><div></div><div></div><div></div><div></div></div>',
+								'<div class="litebox__close"></div>',
+								'<div class="litebox__content"></div>',
+							'</div>',
+						'</div>'].join(''));
+
+			self.$instance = $html.clone();
+			return this;
+		},
 		attach: function(){
-			var self = this;
-			this.elements.each(function(index, element){
-				/*jQuery(element).bind({
-					'change.Disabler': function(){ self.test(element); },
-					'keyup.Disabler': function(){ self.test(element); }
-				});*/
-			});
-		}
+			console.log(this.$elm)
+			this.$elm.on('click.lightbox', function(e){
+				e.preventDefault();
+				console.log(this);
+			})
+			console.log('attached');
+		},
+		open: function(){
+			console.log('open!');
+		},
+		getContent: function(){
+			console.log('getContent')
+		},
+		setContent: function(){
+			console.log('setContent')
+		},
+		close: function (){
+			console.log('close')
+		},
 	};
 
 	// does nothing more than extend jQuery
 	$.fn.litebox = function($content, config){
-		new Litebox($content, config)
-		return this;
+
+		return new Litebox(this, config);
 	};
 
 }(window.DELPHIC || {}, window.jQuery));
