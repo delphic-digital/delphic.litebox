2271d95281f125fc310afa0df03d85bb8106ad26
Jeff <jeffy9@gmail.com>
2015-03-09 17:19:38 -0400

    More OOP progress

--- delphic.litebox.new.js
+++ delphic.litebox.new.js
@@ -16,13 +16,35 @@
 		defaults: {
 			contentFilters: ['image', 'html']
 		},
-		
-		instance: Litebox,
+		constructor: Litebox,
+		root: 'body',
+		targetAttr : 'href',
+
+		contentFilters: {
+			image: {
+				regex: /\.(png|jpg|jpeg|gif|tiff|bmp)(\?\S*)?$/i,
+				process: function(url)  {
+					console.log(url)
+					var self = this,
+						deferred = $.Deferred(),
+						img = new Image();
+					img.onload  = function() { deferred.resolve(
+						$('<img src="'+url+'" alt="" class="'+self.namespace+'-image" />')
+					); };
+					img.onerror = function() { deferred.reject(); };
+					img.src = url;
+					return deferred.promise();
+				}
+			},
+			html: {
+				regex: /^\s*<[\w!][^<]*>/, /* Anything that starts with some kind of valid tag */
+				process: function(html) { return $(html); }
+			}
+		},
 
 		init: function($elm, config){
 			this.$elm = $elm;
 			this.config = $.extend({}, this.defaults, config);
-			console.log('init');
 
 			var self = this,
 			    $html = $([
@@ -35,20 +57,71 @@
 						'</div>'].join(''));
 
 			self.$instance = $html.clone()
-			return this;
+			return self;
 		},
-		
+
 		attach: function(){
-			var lb = this;
-			this.$elm.on('click.lightbox', function(e){
+			var self = this;
+			self.$elm.on('click.lightbox', function(e){
 				e.preventDefault();
-				console.log(lb);
-				lb.open();
-			})		
-		}, 
-		
-		open: function(){
-			console.log('open now');
+				self.$target = $(this);
+				self.open();
+			})
+		},
+
+		getContent: function(){
+			DEBUG && console.log('getContent');
+			var self = this,
+				filters = this.contentFilters,
+				filter = null,
+				readTargetAttr = function(name){ return self.$target && self.$target.attr(name); },
+				data = readTargetAttr(self.targetAttr),
+				target = data;
+
+			$.each(filters, function() {
+				filter = this;
+
+				if(filter.regex && target.match && target.match(filter.regex)) {
+					data = target;
+				}
+				return !data;
+			});
+
+			//Process it
+			return filter.process.call(self, data);
+		},
+
+		setContent: function($content){
+			DEBUG && console.log('setContent');
+			var self = this;
+
+			self.$instance.removeClass('loading')
+
+
+			self.$content = $content.addClass('litebox__content');
+			self.$instance.find('.litebox__content').html($content);
+			console.log(self.$instance);
+			return self;
+		},
+
+		open: function(event){
+			DEBUG && console.log('open');
+
+			var self = this,
+			    $content = self.getContent();
+
+			    console.log($content)
+
+			if($content){
+				self.$instance.appendTo(self.root).velocity("fadeIn", { duration: 500 })
+
+				//Set content and show
+				$.when($content).done(function($content){
+					//self.setContent($content);
+				});
+				return self;
+			}
+
 		}
 	}
 	// does nothing more than extend jQuery
